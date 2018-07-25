777f03e59df893211d91b3e1811ca2cfe032007e
Addin Gama Bertaqwa <punyagama@gmail.com>
2018-07-25 11:28:26 +0800

    Profile UI Screen

--- App/Containers/ProfileScreen.js
+++ App/Containers/ProfileScreen.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
-import { ScrollView, Image } from 'react-native'
+import { ScrollView, Image, View } from 'react-native'
 import { connect } from 'react-redux'
+import FastImage from 'react-native-fast-image'
+
+// redux
+import AuthActions from '../Redux/AuthRedux'
 
 // components
 import { StatusBar, UnderDevelopment } from '../Components/General'
@@ -8,6 +12,7 @@ import { StatusBar, UnderDevelopment } from '../Components/General'
 // Styles
 import styles from './Styles/ProfileScreenStyle'
 import { Images } from '../Themes'
+import { Text, ListItem, Divider, Card, Icon } from 'react-native-elements'
 
 class ProfileScreen extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -18,10 +23,39 @@ class ProfileScreen extends Component {
     }
   }
   render () {
+    const { user } = this.props
+    if (!user) {
+      return null
+    }
+    const rightIcon = (
+      <Icon name='chevron-right' />
+    )
+    const logoutIcon = (
+      <Icon name='power' type='feather' />
+    )
     return (
       <ScrollView style={styles.container} contentContainerStyle={{ flexGrow: 1 }}>
         <StatusBar />
-        <UnderDevelopment />
+        <View style={styles.avatarContainer}>
+          <FastImage source={{ uri: user.avatar }} style={styles.avatar} />
+          <Text h4>{user.name}</Text>
+          <Text style={styles.email}>{user.email}</Text>
+        </View>
+        <View style={styles.menuContainer}>
+          <Card containerStyle={styles.card}>
+            <ListItem title='Ubah Avatar' rightIcon={rightIcon} />
+          </Card>
+          <Card containerStyle={styles.card}>
+            <ListItem title='Ubah Password' rightIcon={rightIcon} />
+          </Card>
+          <Card containerStyle={styles.card}>
+            <ListItem title='Ubah Profile' rightIcon={rightIcon} />
+          </Card>
+          <Card containerStyle={styles.card}>
+            <ListItem title='Logout' rightIcon={logoutIcon} onPress={() => this.props.logout()} />
+          </Card>
+
+        </View>
       </ScrollView>
     )
   }
@@ -29,11 +63,13 @@ class ProfileScreen extends Component {
 
 const mapStateToProps = (state) => {
   return {
+    user: state.user.data
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
+    logout: () => dispatch(AuthActions.logout())
   }
 }
 
--- App/Containers/Styles/ProfileScreenStyle.js
+++ App/Containers/Styles/ProfileScreenStyle.js
@@ -1,10 +1,31 @@
 import { StyleSheet } from 'react-native'
-import { Colors, ApplicationStyles } from '../../Themes/'
+import { Colors, ApplicationStyles, Fonts } from '../../Themes/'
 
 export default StyleSheet.create({
   ...ApplicationStyles.screen,
   container: {
     flex: 1,
     backgroundColor: Colors.snow
-  }
+  },
+  avatarContainer: {
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  avatar: {
+    width: 120,
+    height: 120,
+    borderRadius: 75,
+    marginBottom: 20
+  },
+  email: {
+    marginVertical: 5,
+    fontFamily: Fonts.type.emphasis
+  },
+  menuContainer: {
+    padding: 20,
+    backgroundColor: Colors.gray,
+    flex: 1
+  },
+  card: {padding: 0, marginBottom: 5}
 })
